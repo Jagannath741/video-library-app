d544f8f42d84b312c90879048a981a4c777d2b78
Jagannath741 <jagannathpatra718@gmail.com>
2025-07-12 04:23:58 +0530

    Update Files

--- Client/components/VideoHome.jsx
+++ Client/components/VideoHome.jsx
@@ -1,26 +1,23 @@
 import { Link } from "react-router-dom";
-import backgroundImage from "../src/assets/video-library-img.jpg";
+import backgroundImage from "../src/assets/banner.jpg";
 
 export function VideoHome() {
     return (
         <div
-            className="w-screen img bg-cover bg-center flex flex-col justify-center items-center text-white"
+            className=" container-fluid img flex flex-col justify-center items-center text-center"
             style={{ backgroundImage: `url(${backgroundImage})` }}
         >
-            <div className="img1">
+            <div>
                 <h1 style={{ fontFamily: "Poppins, sans-serif", fontSize: "2.5rem", fontWeight: "700", marginBottom: "1.5rem" }}>
                     <span style={{ color: "#facc15" }}>Welcome to </span>
                     <span style={{ color: "#1ae052ff" }}>Video Library</span>
                 </h1>
-
-                <div className="m-5 border-4 border-white p-6 rounded-lg">
-                    <Link  className="btn btn-success border-white text-red  me-5 mx-4" to="/admin-login">
+                    <Link  className="btn btn-success border-white text-red me-3 " to="/admin-login">
                         Admin Login
                     </Link>
-                    <Link className="btn btn-warning border-white text-red mx-4" to="/user-login">
+                    <Link className="btn btn-warning border-white text-red " to="/user-login">
                         User Login
                     </Link>
-                </div>
             </div>
         </div>
     );
